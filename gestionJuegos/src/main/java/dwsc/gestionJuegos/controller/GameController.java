11124c1999a25856ae670fc0011cad6e77fc8d10
Juan Alberto Llopis <p.guillen100@gmail.com>
2019-04-15 19:49:32 +0200

    Realizado: Mejorada interfaz del gestor de noticias. Mejorada interfaz de la tabla de juegos. Realizada creación de juegos. Realizada tabla de jugadores. Realizada creación de jugadores. Realizadas funciones de eliminar juego, jugador y juego y jugador por id.
    Faltante importante.:
    Realizar gestión de errores y estados de devoluciones para creación de juegos, creación de jugadores, eliminación de juegos y eliminación de jugadores.
    Realizar interfaz de eliminar jugador.
    Realizar interfaz de eliminar juego.
    Realizar devolución de feedback al crear, eliminar y editar juego y jugador.
    Realizar funciones e interfaz de editar juego.
    Realizar funciones e interfaz de editar jugador.
    Mejorar interfaz del gestor de noticias.

--- gestionJuegos/src/main/java/dwsc/gestionJuegos/controller/GameController.java
+++ gestionJuegos/src/main/java/dwsc/gestionJuegos/controller/GameController.java
@@ -1,17 +1,30 @@
 package dwsc.gestionJuegos.controller;
 
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.imageio.ImageIO;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import dwsc.gestionJuegos.domain.Game;
 import dwsc.gestionJuegos.repository.GameRepository;
@@ -63,6 +76,69 @@ public class GameController
 		model.put("games", games);
 		
 		return "gameTable";
+	}
+	
+	@RequestMapping("/gamecreation")
+	public String getGameCreation(Map<String, List<Game>> model) 
+	{
+		ArrayList<Game> games = new ArrayList<Game>(gameRepo.findAll());
+		model.put("games", games);
+		
+		return "gamecreation";
+	}
+	
+	@PostMapping("/addGame")
+	public String addGame(Game game, Map<String, List<Game>> model/*, @RequestParam("imageFile") MultipartFile file*/) 
+	{
+		/*ClassLoader classLoader = getClass().getClassLoader();
+		//File fil = new File(classLoader.getResource("static/images").getFile() + "/" + file.getOriginalFilename());
+		Path fileNameAndPath = Paths.get(classLoader.getResource("static/images").getFile() + "/" +file.getOriginalFilename());
+		try 
+		{
+			File fil = new File(classLoader.getResource("static/images").getFile() + "/" +file.getOriginalFilename());
+			//Files.write(fileNameAndPath, file.getBytes());
+		} catch (IOException e) 
+		{
+				e.printStackTrace();
+			}
+		
+		/*try {
+		
+			if (fil.createNewFile()) {
+			    System.out.println("File is created!");
+			} else {
+			    System.out.println("File already exists.");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+
+		gameRepo.save(game);
+		
+		ArrayList<Game> games = new ArrayList<Game>(gameRepo.findAll());
+		model.put("games", games);
+        return "gameTable";
+	}
+	
+	@DeleteMapping("/deleteGame/")
+	public String deletePlayer(Game game, Map<String, List<Game>> model) 
+	{
+		gameRepo.delete(game);
+		
+		ArrayList<Game> games = new ArrayList<Game>(gameRepo.findAll());
+		model.put("games", games);
+        return "gameTable";
+	}
+	
+	@DeleteMapping("/deleteGame/{id}")
+	public String deleteGameById(@PathVariable int id, Map<String, List<Game>> model) 
+	{
+		gameRepo.deleteById(id);
+		
+		ArrayList<Game> games = new ArrayList<Game>(gameRepo.findAll());
+		model.put("games", games);
+        return "gameTable";
 	}
 
 }
--- gestionJugadores/src/main/java/dwsc/gestionJugadores/controller/PlayerController.java
+++ gestionJugadores/src/main/java/dwsc/gestionJugadores/controller/PlayerController.java
@@ -3,18 +3,22 @@ package dwsc.gestionJugadores.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dwsc.gestionJugadores.domain.Player;
 import dwsc.gestionJugadores.repository.PlayerRepository;
 
-@RestController
+@Controller
 public class PlayerController 
 {
 	@Autowired
@@ -49,6 +53,54 @@ public class PlayerController
 		}
 		
 	}*/
+	
+	@RequestMapping("/playerstable")
+	public String getPlayersTab(Map<String, List<Player>> model) 
+	{
+		ArrayList<Player> players = new ArrayList<Player>(playerRepo.findAll());
+		model.put("players", players);
+		
+		return "playerTable";
+	}
+	
+	@RequestMapping("/playercreation")
+	public String getPlayerCreation(Map<String, List<Player>> model) 
+	{
+		ArrayList<Player> players = new ArrayList<Player>(playerRepo.findAll());
+		model.put("players", players);
+		
+		return "playercreation";
+	}
+	
+	@PostMapping("/addPlayer")
+	public String addPlayer(Player player, Map<String, List<Player>> model) 
+	{
+		playerRepo.save(player);
+		
+		ArrayList<Player> players = new ArrayList<Player>(playerRepo.findAll());
+		model.put("players", players);
+        return "playerTable";
+	}
+	
+	@DeleteMapping("/deletePlayer/")
+	public String deletePlayer(Player player, Map<String, List<Player>> model) 
+	{
+		playerRepo.delete(player);
+		
+		ArrayList<Player> players = new ArrayList<Player>(playerRepo.findAll());
+		model.put("players", players);
+        return "playerTable";
+	}
+	
+	@DeleteMapping("/deletePlayer/{id}")
+	public String deletePlayerById(@PathVariable int id, Map<String, List<Player>> model) 
+	{
+		playerRepo.deleteById(id);
+		
+		ArrayList<Player> players = new ArrayList<Player>(playerRepo.findAll());
+		model.put("players", players);
+        return "playerTable";
+	}
 
 }
 
